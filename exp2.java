51864aae00fa1f8b6adfb6143cb486b6a59768d8
shakunt17 <118207882+shakunt17@users.noreply.github.com>
2022-11-14 23:02:35 +0530

    Create exp2.java

---
+++ exp2.java
@@ -0,0 +1,18 @@
+public class exp2 {
+    public static void main(String[] args) {
+        int i,j;
+   
+        System.out.println("the prime numbers from 1 to 1000 are:");
+        for (i = 1; i <=1000; i++) {
+            int count=0;
+             for(j=i;j>0;j--){
+                if(i%j==0){
+                   count++;
+                }
+             }
+             if(count==2){
+                System.out.println(i);
+             }
+        }
+    }
+}
